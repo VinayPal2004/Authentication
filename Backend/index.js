ef17c38da8a3324d0fbb115e9d6a0a396d6f2712
VinayPal2004 <vpal92780@gmail.com>
2026-04-03 17:02:25 +0530

    deploy frontend / backend

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://service-hub-plum.vercel.app"
+    "https://service-hub-wrdt.vercel.app/"
   ],
   credentials: true
 }));
