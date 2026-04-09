f63e4af075c8584805ef138a45a763cd8070d303
VinayPal2004 <vpal92780@gmail.com>
2026-04-09 17:58:40 +0530

    complete, update edit profile

--- frontend/our factory/src/components/editprofile.jsx
+++ frontend/our factory/src/components/editprofile.jsx
@@ -31,11 +31,11 @@ function EditProfile() {
       setForm({
         name: userData.name || "",
         Phone: userData.Phone || "",
-          address: userData.address.full || "",
-          city: userData.address.city || "",
-          state: userData.address.state || "",
-          country: userData.address.country || "",
-          pincode: userData.address.pincode || "",
+          address: userData.address?.full || "",
+          city: userData.address?.city || "",
+          state: userData.address?.state || "",
+          country: userData.address?.country || "",
+          pincode: userData.address?.pincode || "",
           theme: userData.theme || "dark",
       });
 
